8bda6fb0e81837be19427195024ec767dd5e52c3
Md. Abu Bakkar Siddiq <95091927+abu-bakkar-sebpo@users.noreply.github.com>
2023-08-13 01:17:57 +0600

    show data in frontend

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-
+from . import models
 # Create your views here.
 def home(request):
-    return render(request,"home.html")
+    student = models.Student.objects.all()
+    print(student)
+    return render(request,"home.html",{"studentData":student})
